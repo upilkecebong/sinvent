8c9bbc3d772275a8dc455f2db2c7fbf71b160052
upilkecebong <upilkecebong24@gmail.com>
2024-06-13 09:05:09 +0700

    config barang keluar controller

--- app/Http/Controllers/BarangKeluarController.php
+++ app/Http/Controllers/BarangKeluarController.php
@@ -2,63 +2,137 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
+use App\Models\BarangKeluar;
+use App\Models\BarangMasuk;
 use Illuminate\Http\Request;
+use DB;
 
 class BarangKeluarController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $rsetBarangKeluar = BarangKeluar::with('barang')->latest()->paginate(10);
+        return view('barangkeluar.index', compact('rsetBarangKeluar'))
+            ->with('i', (request()->input('page', 1) - 1) * 10);
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
+    
     public function create()
     {
-        //
+        $abarangkeluar = Barang::all();
+        return view('barangkeluar.create',compact('abarangkeluar'));
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'tgl_keluar'   => 'required|date',
+            'qty_keluar'   => 'required|numeric|min:1',
+            'barang_id'    => 'required|exists:barang,id',
+        ]);
+
+        $tgl_keluar = $request->tgl_keluar;
+        $barang_id = $request->barang_id;
+
+        // Check if there's any BarangMasuk with a date later than tgl_keluar
+        $existingBarangMasuk = BarangMasuk::where('barang_id', $barang_id)
+            ->where('tgl_masuk', '>', $tgl_keluar)
+            ->exists();
+    
+        if ($existingBarangMasuk) {
+            return redirect()->back()->withInput()->withErrors(['tgl_keluar' => 'Tanggal keluar tidak boleh mendahului tanggal masuk!']);
+        }
+    
+        $barang = Barang::find($barang_id);
+
+        //Validasi jika jumlah qty_keluar lebih besar dari stok saat itu, muncul pesan eror
+        if ($request->qty_keluar > $barang->stok) {
+            return redirect()->back()->withInput()->withErrors(['qty_keluar' => 'Jumlah barang keluar melebihi stok!']);
+        }
+
+        elseif ($request->qty_keluar < 1) {
+            return redirect()->back()->withInput()->withErrors(['qty_keluar' => 'Jumlah barang keluar tidak boleh 0!']);
+        }
+
+        //create post
+        BarangKeluar::create([
+            'tgl_keluar'        => $request->tgl_keluar,
+            'qty_keluar'        => $request->qty_keluar,
+            'barang_id'        => $request->barang_id,
+        ]);
+
+        return redirect()->route('barangkeluar.index')->with(['success' => 'Data Barang Keluar Berhasil Disimpan!']);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        $rsetBarangKeluar = BarangKeluar::find($id);
+
+
+        // Delete the main record in barang table
+        $rsetBarangKeluar->delete();
+
+        // Redirect to index
+        return redirect()->route('barangkeluar.index')->with(['success' => 'Data Barang Keluar Berhasil Dihapus!']);
     }
+
+    public function show(string $id)
+    {
+        $rsetBarangKeluar = BarangKeluar::find($id);
+
+        //return view
+        return view('barangkeluar.show', compact('rsetBarangKeluar'));
+    }
+
+    public function edit($id)
+    {
+        $rsetBarangKeluar = BarangKeluar::findOrFail($id);
+        $abarangkeluar = Barang::all();
+        $selectedBarang = Barang::find($rsetBarangKeluar->barang_id); 
+        return view('barangkeluar.edit', compact('rsetBarangKeluar', 'abarangkeluar', 'selectedBarang'));    
+    }
+
+    public function update(Request $request, $id)
+    {
+
+        $request->validate( [
+            'tgl_keluar'   => 'required|date',
+            'qty_keluar'   => 'required|numeric|min:1',
+            'barang_id'    => 'required|exists:barang,id',
+        ]);
+
+        $tgl_keluar = $request->tgl_keluar;
+        $barang_id = $request->barang_id;
+
+        // Check if there's any BarangMasuk with a date later than tgl_keluar
+        $existingBarangMasuk = BarangMasuk::where('barang_id', $barang_id)
+            ->where('tgl_masuk', '>', $tgl_keluar)
+            ->exists();
+    
+        if ($existingBarangMasuk) {
+            return redirect()->back()->withInput()->withErrors(['tgl_keluar' => 'Tanggal keluar tidak boleh mendahului tanggal masuk!']);
+        }
+    
+        $barang = Barang::find($barang_id);
+
+
+        //Validasi jika jumlah qty_keluar lebih besar dari stok saat itu, muncul pesan eror
+        if ($request->qty_keluar > $barang->stok) {
+            return redirect()->back()->withInput()->withErrors(['qty_keluar' => 'Jumlah barang keluar melebihi stok!']);
+        }
+
+        elseif ($request->qty_keluar < 1) {
+            return redirect()->back()->withInput()->withErrors(['qty_keluar' => 'Jumlah barang keluar tidak boleh 0!']);
+        }    
+
+        //create post
+        $rsetBarangKeluar = BarangKeluar::findOrFail($id);
+            $rsetBarangKeluar->update([
+                'tgl_keluar' => $request->tgl_keluar,
+                'qty_keluar' => $request->qty_keluar,
+                'barang_id' => $request->barang_id,
+            ]);
+
+        return redirect()->route('barangkeluar.index')->with(['success' => 'Data Berhasil Diupdate!']);
+        }
 }
