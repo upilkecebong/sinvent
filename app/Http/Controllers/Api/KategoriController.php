296b19701e344aa54b6dd694816efd20c1f70ed4
upilkecebong <upilkecebong24@gmail.com>
2024-06-13 09:58:23 +0700

    [invent-04] Hapus Kategori

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -66,6 +66,18 @@ class KategoriController extends Controller
      */
     public function destroy(Kategori $kategori)
     {
-        //
+        $kategori = Kategori::find($id);
+
+        if (!$kategori) {
+            return response()->json(['status' => 'Kategori tidak ditemukan'], 404);
+        }
+        
+        try {
+            $kategori->delete();
+            return response()->json(['status' => 'Kategori berhasil dihapus'], 200);
+        } catch (\Illuminate\Database\QueryException) {
+            // Tangkap pengecualian spesifik dari database (termasuk constraints foreign key)
+            return response()->json(['status' => 'Kategori tidak dapat dihapus'], 500);
+        }
     }
 }
